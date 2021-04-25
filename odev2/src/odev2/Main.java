efe7d8745b22e43280edc7cb66129e6525c6c140
akykaan <43580230+akykaan@users.noreply.github.com>
2021-04-25 15:19:05 +0300

    Update Main.java

--- odev2/src/odev2/Main.java
+++ odev2/src/odev2/Main.java
@@ -5,20 +5,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Courses courses1=new Courses
-				("\\ımage\\coursesImage1.jpg", "Java Kampı", "Engin Demiroğ", 0);
+				("\\image\\coursesImage1.jpg", "Java KampÄ±", "Engin DemiroÄŸ", 0);
 		
 		Courses courses2=new Courses
-				("\\ımage\\coursesImage1.jpg", "C# Kampı", "Engin Demiroğ", 0);
+				("\\image\\coursesImage1.jpg", "C# KampÄ±", "Engin DemiroÄŸ", 0);
 		
 		Courses courses3=new Courses
-				("\\ımage\\coursesImage1.jpg", "Flutter Kampı", "Engin Demiroğ", 0);
+				("\\image\\coursesImage1.jpg", "Flutter KampÄ±", "Engin DemiroÄŸ", 0);
 		
 		Courses courses4=new Courses
-				("Unity Kampı", "Engin Demiroğ", 0);
+				("Unity KampÄ±", "Engin DemiroÄŸ", 0);
 		
 		
 		Courses courses5=new Courses
-				("C++ Kampı", "Engin Demiroğ", 0);
+				("C++ KampÄ±", "Engin DemiroÄŸ", 0);
 
 		Courses[] coursesArray= {courses1,courses2,courses3,courses4,courses5};
 		
